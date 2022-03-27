e4fc3ec287846c1f6e53d39c0c15d2dcae81ddd7
asifhaiat <asifhaiat67465@gmail.com>
2022-03-27 20:43:15 +0600

    complete header part

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+
+const header = () => {
+    return (
+        <div className="header">
+            <h1>Your Favorite Store</h1>
+        </div>
+    );
+};
+
+export default header;
