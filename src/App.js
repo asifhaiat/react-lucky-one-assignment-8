84b522228d83f9243ba42e6fae4d5ade3196d963
asifhaiat <asifhaiat67465@gmail.com>
2022-03-27 20:42:02 +0600

    complete app.js structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,71 @@
-import React from 'react';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Main from "./Components/Main/Main";
+import Cart from "./Components/Cart/Cart";
+import Header from "./Components/Header/Header";
 
 
 function App() {
+  const [stores, setStores] = useState([]);
+  const [cart,setCart] = useState([]);
+  <h1>Your Favorite Store</h1>
+  
+
+  useEffect(() => {
+    fetch("items.json")
+      .then((res) => res.json())
+      .then((data) => setStores(data));
+
+  }, []);
+  
+
+  const AddToCartHandler = (store) =>{
+    const isExist = cart.find(item => item.id === store.id)
+    if(!isExist){
+      const newCart = [...cart,store]
+      setCart(newCart);
+      // console.log(newCart);
+    }
+    if(cart.length >= 4){
+      alert('This is out of the cart')
+    }
+    
+    }
+
+    const handleDelete = () => {
+      const newOne = [];
+      setCart(newOne);
+  
+    }
+    const handleUpdate = () => {
+      const random = cart[Math.floor(Math.random()* cart.length)];
+      const newCart = [random]
+      setCart(newCart);
+    
+    }
+    
   return (
-    <div className="App">
+    <div >
+    <div>
+          <Header></Header>
+    </div>
+    <div className="main" >
+    <div className="cart-container">
+      {stores.map((store) => (
+        <Main key={store.id} storeData={store} AddToCartHandler={AddToCartHandler}/>
+      ))}
+      </div>
+      <div>
+        {cart.map((item) => (
+          <Cart key={item.id} cart={item}/>
+        ))}
+        <button onClick={() =>handleUpdate()} className="btn-cart">Choose one for me <small></small> </button>
+        <button onClick={() => handleDelete()} className="btn-cart">Choose again <small></small> </button>
+      </div>
+    </div>
+      
       
     </div>
   );
 }
-
-export default App;
+export default App ;
