d7f22881ced74caf524d1ec06c9789c36c2c0841
asifhaiat <asifhaiat67465@gmail.com>
2022-03-27 20:39:39 +0600

    added fake data and all components folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { CartProvider } from 'react-use-cart';
-import './App.css';
-import Cart from './Components/Cart/Cart';
-import Home from './Components/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <CartProvider>
-      <Home></Home>
-      <Cart></Cart>
-      </CartProvider>
+      
     </div>
   );
 }
